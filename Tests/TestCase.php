b71a8f2f929a14bb7f7e562fd2a37c685415d5e8
c0dex <petrovgm@gmail.com>
2019-02-17 00:23:31 +0300

    Added TestCase

---
+++ Tests/TestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests;
+
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+    public function __construct(?string $name = null, array $data = [], string $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+    }
+
+}
